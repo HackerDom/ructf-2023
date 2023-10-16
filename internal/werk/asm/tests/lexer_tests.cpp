9ac4d1a43fb960a5ca353e0bd063802d293c125b
Roman Khapov <rkhapov@yandex-team.ru>
2023-10-16 22:01:17 +0500

    [werk] some lexer tests

--- internal/werk/asm/tests/lexer_tests.cpp
+++ internal/werk/asm/tests/lexer_tests.cpp
@@ -4,6 +4,7 @@
 
 using namespace werk::assembler;
 using namespace std::literals;
+using werk::assembler::Token;
 
 static std::string tokensToString(const std::vector<Token> &tokens) {
     std::stringstream ss;
@@ -37,4 +38,108 @@ TEST(Lexer, EmptyString) {
 
 TEST(Lexer, OnlyWhitespacesString) {
     assertTokenizeResult("  \n \t    \t\t\t\n\n\n   \n"s, {Token(Token::Type::Eof, "")}, "");
+}
+
+TEST(Lexer, Opcodes) {
+    const char *text = R"(
+mov
+load
+store
+add
+sub
+mul
+setpixel
+getpixel
+clearscreen
+out
+hexout
+cmp
+jmp
+je
+jne
+jl
+jg
+jle
+jge
+call
+ret
+and
+or
+shl
+shr
+)";
+    assertTokenizeResult(std::string(text), {
+            Token(Token::Type::Opcode, "mov"),
+            Token(Token::Type::Opcode, "load"),
+            Token(Token::Type::Opcode, "store"),
+            Token(Token::Type::Opcode, "add"),
+            Token(Token::Type::Opcode, "sub"),
+            Token(Token::Type::Opcode, "mul"),
+            Token(Token::Type::Opcode, "setpixel"),
+            Token(Token::Type::Opcode, "getpixel"),
+            Token(Token::Type::Opcode, "clearscreen"),
+            Token(Token::Type::Opcode, "out"),
+            Token(Token::Type::Opcode, "hexout"),
+            Token(Token::Type::Opcode, "cmp"),
+            Token(Token::Type::Opcode, "jmp"),
+            Token(Token::Type::Opcode, "je"),
+            Token(Token::Type::Opcode, "jne"),
+            Token(Token::Type::Opcode, "jl"),
+            Token(Token::Type::Opcode, "jg"),
+            Token(Token::Type::Opcode, "jle"),
+            Token(Token::Type::Opcode, "jge"),
+            Token(Token::Type::Opcode, "call"),
+            Token(Token::Type::Opcode, "ret"),
+            Token(Token::Type::Opcode, "and"),
+            Token(Token::Type::Opcode, "or"),
+            Token(Token::Type::Opcode, "shl"),
+            Token(Token::Type::Opcode, "shr"),
+            Token(Token::Type::Eof, "")
+    }, "");
+}
+
+TEST(Lexer, LabelDefinition) {
+    const char *text = R"(
+label: mov
+)";
+
+    assertTokenizeResult(std::string(text), {
+            Token(Token::Type::Label, "label"),
+            Token(Token::Type::Colon, ":"),
+            Token(Token::Type::Opcode, "mov"),
+            Token(Token::Type::Eof, "")
+    }, "");
+}
+
+TEST(Lexer, LabelUsage) {
+    const char *text = R"(
+jmp label
+)";
+
+    assertTokenizeResult(std::string(text), {
+            Token(Token::Type::Opcode, "jmp"),
+            Token(Token::Type::Label, "label"),
+            Token(Token::Type::Eof, "")
+    }, "");
+}
+
+TEST(Lexer, OpcodeInsteadOfLabelName) {
+    const char *text = R"(
+jmp moV
+)";
+
+    assertTokenizeResult(std::string(text), {
+            Token(Token::Type::Opcode, "jmp"),
+            Token(Token::Type::Opcode, "moV"),
+            Token(Token::Type::Eof, "")
+    }, "");
+}
+
+TEST(Lexer, Imm) {
+    const char *text = R"(
+1337
+0bcdf
+
+deadbeed
+)";
 }
