3fbbd3449133b0115f100f615b681496c18ba980
root <root@vpn.a>
2023-10-18 08:49:55 +0000

    reduce client size

--- ansible/roles/vpn/gen/gen_conf_server_prod.py
+++ ansible/roles/vpn/gen/gen_conf_server_prod.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-N = 100
+N = 30
 
 SERVER_DATA = """mode p2p
 port {0}
--- ansible/roles/vpn/gen/gen_keys_prod.py
+++ ansible/roles/vpn/gen/gen_keys_prod.py
@@ -2,7 +2,7 @@ import os
 import sys
 from subprocess import call
 
-N = 100
+N = 30
 OPENVPN_PATH = "/usr/sbin/openvpn"
 
 if __name__ != "__main__":
