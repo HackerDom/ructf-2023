b99e228186bc42f18a482b7ce18abd4e08e9ac88
Roman Khapov <rkhapov@yandex-team.ru>
2023-10-30 19:36:34 +0500

    [werk] server -> interpreter_server

--- internal/werk/server/src/run.cpp
+++ internal/werk/server/src/run.cpp
@@ -6,11 +6,13 @@ namespace werk::server {
     utils::result<std::shared_ptr<Run>> Run::CreateFromFile(
             const std::filesystem::path &binaryPath,
             vd_t vd,
-            void *memory) {
+            void *memory,
+            std::uint64_t ticksLimit) {
         return utils::result<std::shared_ptr<Run>>::of_error("not implemented");
     }
 
-    Run::Run(vd_t vd, std::shared_ptr<vm::Vm> vm) : vd(vd), vm(std::move(vm)) {
+    Run::Run(vd_t vd, std::shared_ptr<vm::Vm> vm, std::uint64_t ticksLimit)
+            : vd(vd), vm(std::move(vm)), ticksLimit(ticksLimit) {
         state = Running;
         totalTicksCount = 0;
     }
