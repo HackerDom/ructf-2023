ed24b9f45b443df3f67cd11affb6f24eadc88091
Daniil Sharko <integrex@yandex.ru>
2023-10-31 23:05:56 +0500

    Add db api

--- services/werk/server/impl.go
+++ services/werk/server/impl.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"back/db"
+	"back/models"
 	"context"
 	"fmt"
-
-	"back/models"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type werkServerImpl struct {
 	models.UnimplementedWerkServer
+	dbApi *db.Api
 }
 
 func (s *werkServerImpl) Hello(ctx context.Context, in *models.HelloRequest) (*models.HelloResponse, error) {
 	return &models.HelloResponse{Message: fmt.Sprintf("Hello from WERK, %q!", in.Name)}, nil
+}
+
+func (s *werkServerImpl) Register(ctx context.Context, in *models.CreateUserRequest) (*models.CreateUserResponse, error) {
+	token, err := s.dbApi.CreateUserPair(in.Name)
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, "can not register user")
+	}
+
+	return &models.CreateUserResponse{UserPair: &models.UserPair{
+		Name:  in.Name,
+		Token: token,
+	}}, nil
 }
