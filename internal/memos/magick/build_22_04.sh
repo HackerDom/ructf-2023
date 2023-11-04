804a856708f1f8151ffcc415010c09e14884c8d9
Vladimir <keltecc@ya.ru>
2023-11-04 09:48:45 +0300

    [memos] update service

--- internal/memos/magick/build_22_04.sh
+++ internal/memos/magick/build_22_04.sh
@@ -5,5 +5,6 @@ docker build -f Dockerfile_build_22_04 -t magick_build_22_04 .
 id=$(docker create magick_build_22_04)
 
 docker cp ${id}:/tmp/build/ImageMagick-7.1.1-21/utilities/magick magick_22_04
+docker cp ${id}:/tmp/build/ImageMagick-7.1.1-21/policy.xml policy.xml
 
 docker rm -v ${id}
