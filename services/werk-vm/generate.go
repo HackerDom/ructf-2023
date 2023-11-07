859ce2341fd4df65f03fb896ff7abdbbb345383d
Roman Khapov <rkhapov@yandex-team.ru>
2023-11-07 23:45:51 +0500

    remove trash code

--- services/werk-vm/generate.go
+++
@@ -1,5 +0,0 @@
-package back
-
-//go:generate protoc --go_out=models proto/config.proto
-//go:generate protoc --go_out=models proto/api.proto
-//go:generate protoc --go-grpc_out=models proto/api.proto
